dd42c9d423770cf48ff44ee3a3dc8cf02fa7df2b
Su-Yong <simssy2205@gmail.com>
2018-06-03 13:44:40 +0900

    for sync

--- client/ui/screens/MainScreen.js
+++ client/ui/screens/MainScreen.js
@@ -4,7 +4,7 @@ const ScreenType = require('../ScreenType.js')
 const html = (
 '<div style="">' +
   '<div class="input-provider" style="width: 30vw; ">' + 
-    '<input id="name" style="font-size: 20px;"></input>' +
+    '<input id="name" style="font-size: 20px; width: calc(30vw - 48px)"></input>' +
     '<div id="register" class="flat-button" style="float: right;">GO</div>' +
   '</div>' +
 '</div>'
@@ -13,6 +13,17 @@ const html = (
 class MainScreen extends Screen {
   constructor () {
     super('main', ScreenType.BASE, html)
+  }
+
+  init () {
+    const register = document.getElementById('register')
+    const input = document.getElementById('name')
+
+    register.addEventListener('click', () => {
+      const name = input.value
+
+      
+    })
   }
 }
 
